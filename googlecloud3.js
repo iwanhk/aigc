76b412545ebd409b3a38f449523bfaec82894f5d
Aidan Gao <97348212+hydrus3109@users.noreply.github.com>
2023-08-29 00:17:26 -0400

    3rd version with reset changes

---
+++ googlecloud3.js
@@ -0,0 +1,346 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {getStorage, ref, getDownloadURL, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js" ;
+
+//IMPORTANT API INFO KEEP THIS FOR GOOGLE CLOUD
+const firebaseConfig = {
+  apiKey: "AIzaSyDWlKvENKk3VifFekISeucVGnaUd3PS4LQ",
+  authDomain: "dreambooth-ai.firebaseapp.com",
+  projectId: "dreambooth-ai",
+  storageBucket: "dreambooth-ai.appspot.com",
+  messagingSenderId: "271111459915",
+  appId: "1:271111459915:web:dde6b8c8bdfa23c863e9ce",
+};
+export var inputmod = "people";
+var linklist = [];
+var resultlist=[];
+var finallist=[];
+var count = 0;
+var bglist=["https://firebasestorage.googleapis.com/v0/b/dreambooth-ai.appspot.com/o/test%20backgrounds%2Fchina1.png?alt=media&token=ebaab5aa-5649-4677-9db4-6c5ac8be1373","https://firebasestorage.googleapis.com/v0/b/dreambooth-ai.appspot.com/o/test%20backgrounds%2Fchina2.png?alt=media&token=6982eca4-2a8a-4440-aa32-a35b1ea4a7dc","https://firebasestorage.googleapis.com/v0/b/dreambooth-ai.appspot.com/o/test%20backgrounds%2Fchina3.png?alt=media&token=0628e887-c8cd-44fc-9f3b-f8d2c033eac7"];
+export function maledescriptor(){
+  inputmod = "male";
+  console.log("hi");
+  console.log(inputmod);
+}
+export function femaledescriptor(){
+  inputmod = "female";
+}
+//calls dreambooth api
+async function dreamboothjob(){
+  let response = await fetch('https://api.dreamlook.ai/dreambooth', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'authorization': 'Bearer dl-DB09B60A8F9A4E4089B79B4D5EB8626F'
+    },
+    // body: '{\n        "steps": 925,\n        "learning_rate": 0.000003,\n        "enable_offset_noise": true,\n        "extract_lora": "original",\n        "instance_prompt": "photo of ukj person",\n        "base_model": "realistic-vision-v3-0",\n        "saved_model_format": "original",\n        "saved_model_weights_format": "safetensors",\n        "crop_method": "face",\n        "image_urls": [\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/d918eb1e_7fl9229z_10.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/b62c91d2_0sqjrraw_9.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/b481b705_64yhra1i_8.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/86a8c745_lez8m8dk_7.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/7ce86e77_txm1hply_6.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/3fd572d0_rdgpd42g_5.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/acf116f0_9wmigxr1_4.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/da877ea9_nrd13l3s_3.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/88a5d4f5_bv6yo962_2.jpg",\n            "gs://nyxai-galactus-prd.appspot.com/dreambooth/fLilAfMpduWY16MCWxdDNnH68pz2/2023-7-29-17-15_m4xnsg8t2b/images/4c468b3b_otipzsd7_1.jpg"\n        ]\n    }',
+    body: JSON.stringify({
+      'steps': 825,
+      'learning_rate': 0.000003,
+      'enable_offset_noise': true,
+      'extract_lora': 'disabled',
+      'instance_prompt': 'photo of ukj ' + inputmod,
+      'base_model': 'realistic-vision-v5-1',
+      'saved_model_format': 'original',
+      'saved_model_weights_format': 'safetensors',
+      'crop_method': 'face',
+      'image_urls': linklist, 
+    })
+  })
+  let data = await response.json();
+  data = JSON.stringify(data);
+  data = JSON.parse(data);
+  var toreturn = data['job_id'];
+  return toreturn;
+}
+
+
+var checkpointidgen;
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export async function uploadImages() {
+  //ensure jpeg passed
+  const metadata = {
+    contentType: 'image/jpeg',
+  };
+  //getting file input from user
+  const fileInput = document.getElementById("imageFiles");
+  const files = fileInput.files;
+  if (files.length <10) {
+      alert("Please select  more images to upload.");
+      return;
+  }
+  //storage and unique identifier
+  const storage1 = getStorage();
+  const uploadStatus = document.getElementById("uploadStatus");
+  const identifier = Math.floor(Math.random()*10000);
+  uploadStatus.textContent = "Uploading...";
+  // Loop through each selected image file and upload it to Firebase Storage
+  for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const storageRef = ref(storage1, `images/${identifier}/${file.name}`);
+      console.log(identifier);
+      //uploads to firebase to get direct url for api
+      const uploadTask = await uploadBytesResumable(storageRef, file,metadata);
+      const upload = document.getElementById("stat");
+      upload.textContent = "started";
+      //pushing download urls into list for api
+      var downloadURL=await getDownloadURL(ref(storage1, `images/${identifier}/${file.name}`));
+      linklist.push(downloadURL);
+  }
+
+  console.log(linklist);
+  //getting info from dreambooth job
+  let jobid = await dreamboothjob();
+  console.log(jobid);
+  document.getElementById("id2").textContent = jobid; 
+  alert("done");
+  //db_70c859a2 test job id
+  //status progress bar update
+  document.getElementById('Progress_Status').style.display = 'block'; 
+  var stat = await checkstatus('dreambooth', jobid);
+  while(stat === false){
+    await sleep(5000);
+    stat = await checkstatus('dreambooth', jobid)
+  }
+  //
+  // CHANGE THE LOCATION HERE
+  //
+  //getting dreambooth checkpoint for image gen
+  let checkpointid = await checkpointget(jobid);
+  const upload2 = document.getElementById("final");
+  upload2.textContent = "Image generate started";
+  await generate(checkpointid,"a wild west photo of ukj wearing a cowboy outfit, holstered gun, full body photo, black and white photo, grainy photo, taken with a old camera, shot in the 1700s, natural face, face of a model, looking at camera, award winning photo, detailed clothing, cinematic lighting, "+inputmod);
+  upload2.textContent = "Second Image generate started";
+  await generate(checkpointid,"old photo of ukj as a viking hunter wearing a worn fur cloak and leather tunic, viking helmet, dynamic pose, leather gloves, leather boots, full body, viking hunter, nordic theme, looking at camera, full body, natural face, face of a model, slim face, studio quality, 8k, hdr, smooth, high resolution, award-winning photo, taken in the 1300s, detailed clothing, smooth skin, cinematic lighting, "+inputmod);
+  upload2.textContent = "Third generate started";
+  await generate(checkpointid, "detailed photo of ukj in steampunk mechanical armour, (full body:1.1), Dieselpunk style, steampunk style, Retrofuturism, masterpiece, natural face, face of a model, slim face, photorealistic, detailed background, steampunk blueprint, dynamic pose, baroque ornate, leather, armor, bronze, gold, diamond, (Epic composition, epic proportion, epic fantasy), cinematic light, 8k, front lighting, looking at camera, "+inputmod);
+  upload2.textContent = "Fourth generate started";
+  await generate(checkpointid, "detailed photo of ukj as royalty wearing traditional han chinese clothing, full body, looking at camera, full body, natural face, beautiful face, face of a model, slim face, dynamic pose, matching eyes, studio quality, 8k, hdr, smooth, high resolution, award winning photo, cinematic lighting, looking at the camera, "+inputmod);
+} 
+
+
+export async function checkpointget(jobid){
+  var checkpointid;
+  let response = await fetch(`https://api.dreamlook.ai/jobs/dreambooth/${jobid}`, {
+    headers: {
+      'content-type': 'application/json',
+      'authorization': 'Bearer dl-DB09B60A8F9A4E4089B79B4D5EB8626F'
+    }
+  })
+  let data = await response.json();
+  data = JSON.stringify(data);
+  data = JSON.parse(data);
+  console.log('second API Response:', data); 
+  var data1 = data['dreambooth_result'];
+  checkpointid = data1['checkpoints'][0];
+  checkpointidgen = checkpointid['checkpoint_id'];
+  console.log(checkpointid);
+  console.log(checkpointidgen);
+  return checkpointidgen;
+}
+
+
+//api call to checkstatus of dreambooth/ image gen task
+export async function checkstatus(jobtype, idvar){
+  let response = await fetch(`https://api.dreamlook.ai/jobs/${jobtype}/${idvar}`, {
+    headers: {
+      'content-type': 'application/json',
+      'authorization': 'Bearer dl-DB09B60A8F9A4E4089B79B4D5EB8626F'
+    }
+  })
+  let data = await response.json();
+  data = JSON.stringify(data);
+  data = JSON.parse(data);
+  var percent = data['percentage_done'];
+  console.log('API Response:', data['state']); 
+  console.log('API Response:', data); 
+  if(data['state'] == 'running'){
+    const element = document.getElementById("myprogressBar");   
+    var width = percent; 
+    element.style.width = width + '%'; 
+
+    return false;
+  }
+  if(data['state'] === 'success'){ return true;}
+  else{ return false;}
+
+}
+
+
+//functions to generate images 
+export async function generate(checkpointid, prompt){
+  let response= await fetch('https://api.dreamlook.ai/image_gen', {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+    'authorization': 'Bearer dl-DB09B60A8F9A4E4089B79B4D5EB8626F'
+  },
+  
+  // WILD WEST: a wild west photo of ukj wearing a cowboy outfit, the forbidden palace in background, full body photo, black and white photo, grainy photo, taken with a old camera, shot in the 1700s, natural face, face of a model, looking at camera, award winning photo, detailed clothing, cinematic lighting
+  // 8.5 guidance45
+  // CYBERPUNK: cyberpunk photo of ukj wearing a black leather jacket, nighttime, full body, cyberpunk theme, (forbidden palace in background:1.1), looking at camera, full body, natural face, face of a model, studio quality, 8k, hdr, smooth, high resolution, award winning photo, detailed clothing, smooth skin, cinematic lighting, neon lights, synthwave, detailed background, neon colors, looking at the camera, front lighting, centered
+  // 8.5 guidance
+  //steampunk: detailed photo of ukj in steampunk mech armour, gorgeous, Dieselpunk style, steampunk style, steampunk style forbidden palace in the background, Retrofuturism, masterpiece, natural face, smooth face, detailed face, photorealistic, detailed background, steampunk blueprint, dynamic pose, centered, baroque ornate, leather, armor, bronze, gold, diamond, (Epic composition, epic proportion, epic fantasy), cinematic light, standing in front of the steampunk forbidden palace, detailed sky, 8k
+  // 8.5
+
+  //out of frame, lowres,pixelated, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, ugly face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck
+  body: JSON.stringify({
+    'prompt': prompt,
+    'negative_prompt' : "inside, nudity, out of frame, lowres, pixelated, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, ugly face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, blurry background, misformed buildings",
+    'num_samples': 8,
+    'width': 768,
+    'height': 1024,
+    'num_inference_steps': 20,
+    'enable_hrf': true,
+    'scheduler_type': 'dpm++',
+    'seed': -1,
+    'model_type': 'sd-v1',
+    'guidance_scale': 8,
+    'checkpoint_id': checkpointid
+  })
+})
+let data = await response.json();
+data = JSON.stringify(data);
+data = JSON.parse(data);
+var jobid = data['job_id'];
+var stat = await checkstatus('image-gen', jobid);
+while(stat === false){
+  await sleep(5000);
+  stat = await checkstatus('image-gen', jobid)
+}
+document.getElementById('results').style.display = 'block'; 
+await resultsget(jobid);
+}
+
+
+export async function resultsget(jobid){
+  resultlist=[]
+  var checkpointid;
+  let response = await fetch(`https://api.dreamlook.ai/jobs/image-gen/${jobid}`, {
+    headers: {
+      'content-type': 'application/json',
+      'authorization': 'Bearer dl-DB09B60A8F9A4E4089B79B4D5EB8626F'
+    }
+  })
+  let data = await response.json();
+  data = JSON.stringify(data);
+  data = JSON.parse(data);
+  console.log('third API Response:', data); 
+  var data1 = data['image_gen_results'][0];
+  console.log(data1);
+  var imageurls= data1['generated_images'];
+  console.log(imageurls);
+  for(let i = 0; i < imageurls.length;i++){
+    var temp = imageurls[i];
+    var url = temp['url'];
+    resultlist.push(url);
+  }
+  console.log(resultlist);
+  const container = document.getElementById('image-container2');
+     
+  for (let i = 0; i < resultlist.length; i++) {
+      const img = document.createElement('img');
+      img.src = resultlist[i];
+      img.addEventListener('click', function(){showimg(this.src)});
+      container.appendChild(img);
+  }
+  return resultlist;
+}
+
+
+export function showimg(src){
+  document.getElementById('main').src = src;
+}
+//sleep function for updates
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//test function 
+export async function testgen(){
+  alert("hi");
+
+  document.getElementById('Progress_Status').style.display = 'block'; 
+  document.getElementById('results').style.display = 'block'; 
+  await resultsget("ig_a85d8cf4");
+  await resultsget("ig_31b63551");
+  await resultsget("ig_2e81a9dd");
+  //generate('ckp_b3b42655', "a wild west photo of ukj wearing a cowboy outfit, the forbidden palace in background, full body photo, black and white photo, grainy photo, taken with a old camera, shot in the 1700s, natural face, face of a model, looking at camera, award winning photo, detailed clothing, cinematic lighting");
+}
+
+
+export function updateimgs(){
+  count++;
+  if(count > 3){
+    alert("maximum images selected");
+    return;
+  }
+  console.log(count);
+  finallist.push(document.getElementById('main').src);
+  console.log(finallist);
+  const container = document.getElementById('image-container3');
+  const img = document.createElement('img');
+  img.src=document.getElementById('main').src;
+  container.appendChild(img);
+}
+
+
+export async function bgswap(){
+  const container = document.getElementById('finalimgs');
+  for(var i = 0; i < 3; i++){
+    console.log(i);
+    let finurl = await apicall(finallist[i], bglist[i]);
+    const img = document.createElement('img');
+    img.src = finurl;
+    container.appendChild(img);
+  }
+}
+export async function reset(){
+  const container = document.getElementById('image-container3');
+  container.innerHTML='';
+  count=0;
+  finallist=[];
+  alert('reset choices');
+}
+
+async function apicall(imgurl, bgurl){
+  const form = new FormData();
+  form.append('output_type', 'cutout');
+  form.append('bg_blur', '0');
+  form.append('scale', 'fit');
+  form.append('format', 'PNG');
+  form.append('image_url', imgurl);
+  form.append('bg_image_url', bgurl);
+  let response = await fetch("https://api.picsart.io/tools/1.0/removebg",{
+    method: "POST",
+    headers: {
+      "x-picsart-api-key": "vjfYuCNcan0v4Ks6a5gybGARzWXAHngS",
+      'accept': 'application/json',
+    },
+    body: form
+  })
+  console.log(response);
+  let data = await response.json(); 
+  data = JSON.stringify(data);
+  data = JSON.parse(data);
+  console.log('bgAPI response', data); 
+  var data1 = data["data"];
+  var data2 = data1["url"];
+  return data2;
+}
+export const pls = document.getElementById("pls");
+pls.addEventListener('click', uploadImages, false);
+//uploadImages
+//generate('ckp_3ab44e7a')
+//export const gen = document.getElementById("hiddenButton");
+//gen.addEventListener('click', generate, false);
+export const malebutton = document.getElementById("male");
+malebutton.addEventListener('click', maledescriptor);
+export const femalebutton = document.getElementById("female");
+femalebutton.addEventListener('click', femaledescriptor);
+export const imageclick = document.getElementById("main");
+imageclick.addEventListener('click', updateimgs, false);
+export const submitimgs = document.getElementById("finalsubmit");
+submitimgs.addEventListener('click', bgswap, false);
+export const resetimgs = document.getElementById("reset");
+resetimgs.addEventListener('click', reset, false);
